977edbc77abc3c332dd918a8bc6facd79db66409
taichiayin <taichiayin@livemail.tw>
2020-07-03 00:24:21 +0800

    ignore

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/mywebsite/" : "/",
-  baseUrl: '/mywebsite/'
+  publicPath: process.env.NODE_ENV === "production" ? "/mywebsite/" : "/"
 }
