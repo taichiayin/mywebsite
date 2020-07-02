acafb93013ecaef55ebce9de176f154e5ba472bd
taichiayin <taichiayin@livemail.tw>
2020-07-03 00:22:05 +0800

    set baseUrl gitHub pages 404

--- src/router/index.js
+++ src/router/index.js
@@ -18,8 +18,7 @@ const routes = [
 
 const router = new VueRouter({
     mode: 'history',
-    // base: process.env.BASE_URL,
-    base: '/mywebsite/',
+    base: process.env.BASE_URL,
     routes
 })
 
