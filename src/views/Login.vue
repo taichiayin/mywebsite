18bbb920e7bf0d437d276f12da4526a4e0e0e167
taichiayin <37952056+taichiayin@users.noreply.github.com>
2020-07-03 22:57:51 +0800

    update login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,27 +1,29 @@
 <template>
-  <div class="login">
-    <div class="wrap">
-      <h3>Sign in</h3>
-      <div class="item">
-        <div class="label">Username</div>
-        <input type="text" />
-      </div>
-      <div class="item">
-        <div class="label">Password</div>
-        <input type="text" />
-        <div class="eye">
-          <i class="fas fa-eye-slash"></i>
-          <i class="fas fa-eye"></i>
-        </div>
-      </div>
-      <button class="login">Sign in</button>
-      <div class="other">
-        <div class="signup">Sign up</div>
-        <div class="divider">|</div>
-        <div class="forgetpassword">Forget Password</div>
-      </div>
-    </div>
-  </div>
+	<div class="login">
+		<div class="wrap">
+			<div class="item">
+				<h3>Sign in</h3>
+			</div>
+			<div class="item">
+				<input type="text" required />
+				<div class="label">Username</div>
+			</div>
+			<div class="item">
+				<input type="password" required />
+				<div class="label">Password</div>
+				<div class="eye">
+					<i class="fas fa-eye-slash"></i>
+					<i class="fas fa-eye"></i>
+				</div>
+			</div>
+			<button class="signin">Sign in</button>
+			<div class="other">
+				<div class="signup">Sign up</div>
+				<div class="divider">|</div>
+				<div class="forgetpassword">Forget Password</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -30,72 +32,101 @@ export default {}
 
 <style lang="scss" scoped>
 .login {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100vh;
-  background-image: url('../assets/img/login_bg.jpg') ;
-  background-repeat: no-repeat;
-  background-size: cover;  
-  .wrap {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 90%;
-    max-width: 600px;
-    height: 800px;
-    background-color: rgba(0,0,0,.7);
-    border: 10px solid #888;
-    backdrop-filter: blur(5px);
-  }
-  h3 {
-    font-size: 36px;
-    font-weight: 700;
-    padding: 20px;
-  }
-  .item {
-    display: flex;
-    width: 70%;
-    margin: 10px;
-    position: relative;
-    .label {
-      position: absolute;
-      top: 50%;
-      left: 0;
-      transform: translateY(-50%);
-    }
-    .eye {
-      position: absolute;
-      top: 50%;
-      right: 5px;
-      transform: translateY(-50%);
-    }
-    input {
-      width: 100%;
-      height: 30px;
-    }
-  }
-  .login {
-    width: 70%;
-    height: 30px;
-    margin: 10px;
-  }
-  .other {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 10px;
-    .signup {
-      padding: 5px;
-    }
-    .divider {
-      font-weight: 600;
-    }
-    .forgetpassword {
-      padding: 5px;
-    }
-  }
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	height: 100vh;
+	background-image: url('../assets/img/login_bg.jpg');
+	background-repeat: no-repeat;
+	background-size: cover;
+	font-family: 'Anton', sans-serif;
+  
+	.wrap {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		width: 90%;
+		max-width: 600px;
+		height: 800px;
+		background-color: rgba(100, 150, 150, 0.7);
+		border: 10px solid #888;
+		backdrop-filter: blur(5px);
+    box-shadow: 1px 1px 5px rgba(50, 50, 50, .7);
+	}
+	h3 {
+		font-size: 57px;
+		color: #fff;
+	}
+	.item {
+		display: flex;
+		width: 70%;
+		margin: 10px;
+		position: relative;
+		.label {
+			text-transform: uppercase;
+			position: absolute;
+			font-size: 14px;
+			top: 50%;
+			left: 5px;
+			transform: translateY(-50%);
+			color: #ccc;
+			pointer-events: none;
+			user-select: none;
+			letter-spacing: 2px;
+		}
+		.eye {
+			position: absolute;
+			top: 50%;
+			right: 5px;
+			transform: translateY(-50%);
+			font-size: 22px;
+			color: #ccc;
+			cursor: pointer;
+		}
+		input {
+			width: 100%;
+			height: 70px;
+			font-size: 18px;
+			padding: 14px 0 0 5px;
+			background-color: transparent;
+			border-bottom: 1px solid #ccc;
+			font-family: 'Anton', sans-serif;
+			&:focus ~ .label,
+			&:valid ~ .label {
+				top: 15px;
+			}
+		}
+	}
+
+	.signin {
+		width: 70%;
+		height: 50px;
+		padding: 0 0 5px 0;
+		margin: 10px;
+		font-size: 20px;
+		color: rgba(100, 150, 150, 1);
+		background-color: #fff;
+		cursor: pointer;
+		font-family: 'Notable', sans-serif;
+    box-shadow: 1px 1px 5px rgba(50, 100, 100, 1);
+	}
+	.other {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 10px;
+    color: #fff;
+    letter-spacing: 1px;
+		.signup {
+			padding: 5px;      
+		}
+		.divider {
+		}
+		.forgetpassword {
+			padding: 5px;
+		}
+	}
 }
 </style>
